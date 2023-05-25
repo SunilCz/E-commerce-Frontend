768033a3fd9bd2373aa07e3d73388efce79146db
SunilCz <sunil.chaudhary7281@gmai.com>
2023-05-25 22:29:27 +0545

    Stripe update

--- pages/cart.js
+++ pages/cart.js
@@ -8,8 +8,7 @@ import { useSelector } from "react-redux";
 import { makePaymentRequest } from "@/utils/api";
 import { loadStripe } from "@stripe/stripe-js";
 const stripePromise = loadStripe(
-    process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
-);
+    process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
 
 const Cart = () => {
     const [loading, setLoading] = useState(false);
